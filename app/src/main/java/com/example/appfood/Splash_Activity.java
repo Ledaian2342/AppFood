69985b179b1bc523568a33a6a20ca1d23152f9f5
Chuan <chuanntph14556@fpt.edu.vn>
2022-05-26 00:43:28 +0700

    làm xong màn hình Login

--- app/src/main/java/com/example/appfood/Splash_Activity.java
+++ app/src/main/java/com/example/appfood/Splash_Activity.java
@@ -2,13 +2,24 @@ package com.example.appfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Splash_Activity extends AppCompatActivity {
-
+    Button btnnext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
+        btnnext = findViewById(R.id.button);
+        btnnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dashboard = new Intent(Splash_Activity.this, Login_Activity.class);
+                startActivity(dashboard);
+            }
+        });
     }
 }
